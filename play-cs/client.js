c264278ac1fc4f75b460f96ad7073ffbddbc481d
bazawew <106189440+bazawew@users.noreply.github.com>
2023-08-05 14:18:08 +0300

    longjump edit 3

--- play-cs/client.js
+++ play-cs/client.js
@@ -11818,8 +11818,8 @@
 								c[i + 200 >> 2] = e | 2; //pmove->oldbuttons
 								break
 							}
-							//if(!(e & 2)) {
-							if(true) {
+							if(!(e & 2)) {
+							//if(true) {
 								if(c[i + 144 >> 2] | 0 ? c[i + 184 >> 2] & 16384 | 0 : 0) break; 
 								Fk(); //__Z24PM_CatagorizeTextureTypev
 								c[(c[n + 64500 >> 2] | 0) + 224 >> 2] = -1; //pmove->onground
@@ -11902,8 +11902,8 @@
 								if(f > 0.0) g[i + 100 >> 2] = h * (100.0 - f * .001 * 19.0) * .01; //pmove->velocity[2]
 								g[i + 528 >> 2] = 1315.7894287109375; //pmove->fuser2
 								Lr(); //__Z23PM_FixupGravityVelocityv
-								j = (c[n + 64500 >> 2] | 0) + 200 | 0; //pmove->oldbuttons
-								c[j >> 2] = c[j >> 2] | 2
+								//j = (c[n + 64500 >> 2] | 0) + 200 | 0; //pmove->oldbuttons
+								//c[j >> 2] = c[j >> 2] | 2
 							}
 						} else {
 							c[i + 200 >> 2] = c[i + 200 >> 2] | 2; //pmove->oldbuttons (if dead no jumps)
