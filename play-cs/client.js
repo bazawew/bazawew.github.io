b1d01fa9f29bc860525bc14fbe1f28ceb44b4152
bazawew <106189440+bazawew@users.noreply.github.com>
2026-02-06 07:05:00 +0300

    Add files via upload

--- play-cs/client.js
+++ play-cs/client.js
@@ -36001,7 +36001,7 @@
 								puzo[2] + viewvec[2] * 200.0
 							];
 							let enddot = w2s(endview);
-							if (enddot != null && aenddot != 0){
+							if (enddot != null && enddot != 0){
 								drawThinLine(puzodot[0], puzodot[1], enddot[0], enddot[1], '#700f81');
 							}
 						}
